c36b1cfb9bbcbdbcc5159633ffa60a72ae6c9f8c
nabeeljarral <40709574+nabeeljarral@users.noreply.github.com>
2026-02-07 09:12:53 +0500

    Create VPC configuration

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+}
